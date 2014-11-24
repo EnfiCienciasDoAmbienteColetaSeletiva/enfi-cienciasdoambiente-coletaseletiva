2158cb6622ad646e1162516a2899071029a434d2
Rafael Santos <rafael.santos.193@gmail.com>
2014-11-24 03:56:07 +0000

    Adicionado Slider para radio máximo

--- src/Enfi/CienciasDoAmbiente/SiteBundle/Controller/DefaultController.php
+++ src/Enfi/CienciasDoAmbiente/SiteBundle/Controller/DefaultController.php
@@ -26,7 +26,7 @@ class DefaultController extends Controller
         $form = $this->createFormBuilder($encontrarPontoDeColeta)
             ->add('tipoDeLixo', 'entity', array('class' => 'Enfi\CienciasDoAmbiente\CommonEntitiesBundle\Entity\TipoDeLixo', 'property' => 'nome', 'label' => 'Tipo de lixo'))
             ->add('endereco', null, array('label' => 'Endereço ou CEP'))
-            ->add('distancia_maxima', null, array('label' => 'Raio de busca (em km)'))
+            ->add('distancia_maxima', 'genemu_jqueryslider', array('label' => 'Raio de busca: ', 'max' => '30'))
             ->add('submit', 'submit', array('label' => 'Encontre o ponto de coleta mais próximo!'))
             ->getForm();
 
